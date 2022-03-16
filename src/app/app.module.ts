b9179f7cf0aa026b198cab85275300d63e76781d
Cathryn Ackerman <catacker46@gmail.com>
2022-03-16 10:43:19 +0200

    add home & login screen components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './components/login-screen/login-screen.component';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent,
+    HomeScreenComponent
   ],
   imports: [
     BrowserModule,
